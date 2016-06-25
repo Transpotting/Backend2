743c1fcbe2bee8ad109fa74a8004d56a8d899f2e
Ivan Voras <ivoras@gmail.com>
2016-06-26 01:42:00 +0200

    Position updating API works

--- web/bibo/admin.py
+++ web/bibo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Zone)
+admin.site.register(Point)
+admin.site.register(Line)
+admin.site.register(LinePoint)
+admin.site.register(Vehicle)
+admin.site.register(Beacon)
+admin.site.register(VehiclePosition)
+admin.site.register(Profile)
+admin.site.register(ProfileInVehicle)
+
--- web/bibo/views.py
+++ web/bibo/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
 
-# Create your views here.
+def index(req):
+    ctx = {}
+    return render(req, 'bibo/index.html', ctx)
+
+
+@csrf_exempt
+def api_update_position(req):
+    result = {}
+    if req.method == 'POST':
+        name = req.POST['name']
+        lat = float(req.POST['lat'])
+        lng = float(req.POST['long'])
+
+        beacon = Beacon.objects.get(name = name)
+        vehicle = Vehicle.objects.get(beacon = beacon)
+
+        result['ok'] = True
+        result['vehicle_id'] = vehicle.id
+        
+        vp = VehiclePosition(beacon = beacon, lat = lat, lng = lng)
+        vp.save()
+
+    else:
+        result['ok'] = False
+        result['msg'] = "Unsupported method"
+
+    return JsonResponse(result)
+
